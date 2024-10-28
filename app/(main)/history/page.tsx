7fa0b4672acc26748ffad39d1ae59036cbf11c34
Spike433 <juhacar116@gmail.com>
2024-10-28 18:41:36 +0100

    Add basic filter functionality to history page

--- app/(main)/history/page.tsx
+++ app/(main)/history/page.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
+"use client";
 
-const EmptyPage = () => {
+import React, { useState } from 'react';
+import { FilterMatchMode } from 'primereact/api';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+
+export default function BasicFilterDemo() {
+    const processes = [
+        { id: 1000, name: 'Process A', startTime: '2024-10-22T15:05:20', duration: 9000 },
+        { id: 1001, name: 'Process B', startTime: '2024-10-22T16:00:00', duration: 6300 },
+        { id: 1002, name: 'Process C', startTime: '2024-10-22T17:15:00', duration: 11700 },
+    ];
+
+    const [globalFilter, setGlobalFilter] = useState<string | null>(null);
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    });
+    const [loading, setLoading] = useState(false);
+
+    const graphBodyTemplate = () => {
+        return <Button icon="pi pi-chart-line" className="p-button-text p-button-plain" />;
+    };
+    const printerBodyTemplate = () => {
+        return <Button icon="pi pi-print" className="p-button-text p-button-plain" />;
+    };
+
     return (
-        <div className="grid">
-            <div className="col-12">
-                <div className="card">
-                    <h5>Empty Page</h5>
-                    <p>Use this page to start from scratch and place your custom content.</p>
-                </div>
-            </div>
+        <div className="card">
+            <h2>Povijest procesa</h2>            
+            <DataTable value={processes} paginator rows={10} dataKey="id" filters={filters} filterDisplay="row" loading={loading}
+                >
+                <Column field="name" header="Ime procesa" filter filterPlaceholder="Search by name" style={{ maxWidth: '250px' }} />
+                <Column field="startTime" header="Vrijeme pocetka" style={{ maxWidth: '200px' }} />
+                <Column field="duration" header="Duljina trajanja (s)"  bodyStyle={{ textAlign: 'center' }} />
+                <Column body={graphBodyTemplate} header="Graf" style={{ maxWidth: '100px' }} />
+                <Column body={printerBodyTemplate} header="Printanje" style={{ maxWidth: '100px' }} />
+            </DataTable>
         </div>
     );
-};
-
-export default EmptyPage;
+}
