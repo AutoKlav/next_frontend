27356c2584e9c22cf6225624ef24e763fc6decd8
Spike433 <juhacar116@gmail.com>
2024-10-29 14:52:26 +0100

    Refactor chart and dashboard pages

---
+++ demo/components/StatusHeader/StatusHeader.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Tag } from "primereact/tag";
+
+const StatusHeader = () => {
+    return (
+        <div className="grid">
+            <div className="col-4">
+                <div className="card mb-0">
+                    <div className="flex justify-content-between mb-3">
+                        <div>
+                            <Tag className="mb-3" icon="pi pi-check" severity="success" value="Proces je završio"></Tag>
+                            <div className="text-900 font-medium text-xl">Grah 500g</div>
+                        </div>                        
+                    </div>                    
+                    <span className="text-500">Ime konzerve</span>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="card mb-0 h-full">
+                    <div className="mb-3 mt-5">
+                        <div>
+                            <div className="text-900 font-medium text-xl">01:30:30</div>
+                        </div>
+                    </div>                    
+                    <span className="text-500">Završeno vrijeme</span>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="card mb-0 h-full">                        
+                    <div className="mb-3 mt-5">
+                        <div>                            
+                            <div className="text-900 font-medium text-xl">00:30:00</div>
+                        </div>                        
+                    </div>                    
+                    <span className="text-500">Predviđen kraj</span>
+                </div>
+            </div>            
+            <div className="col-4">
+                <div className="card mb-0">
+                    <div className="flex justify-content-between mb-">
+                        <div>
+                            <Tag className="mb-3" icon="pi pi-info-circle" severity="info" value="Spremno za početak"></Tag>
+                            <div className="text-900 font-medium text-xl">Grah 500g</div>
+                        </div>                        
+                    </div>                    
+                    <span className="text-500">Ime konzerve</span>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="card mb-0">
+                    <div className="flex justify-content-between mb-3">
+                        <div>                            
+                            <Tag className="mb-3" icon="pi pi-exclamation-triangle" severity="warning" value="Proces je u izvođenju"></Tag>
+                            <div className="text-900 font-medium text-xl">Grah 500g</div>
+                        </div>                        
+                    </div>                    
+                    <span className="text-500">Ime konzerve</span>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="card mb-0">
+                    <div className="flex justify-content-between mb-3">
+                        <div>                            
+                            <Tag className="mb-3" icon="pi pi-times" severity="danger" value="Greška u izvođenju"></Tag>
+                            <div className="text-900 font-medium text-xl">Grah 500g</div>
+                        </div>                        
+                    </div>                    
+                    <span className="text-500">Ime konzerve</span>
+                </div>
+            </div>            
+        </div>
+    );
+};
+
+export default StatusHeader;
