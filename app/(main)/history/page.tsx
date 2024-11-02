fbc99e2bdb079d3ac27f33de1ef60fc219807616
Spike433 <juhacar116@gmail.com>
2024-11-02 13:36:17 +0100

    Update DataTable and FilterMeta imports and add new filter functionality

--- app/(main)/history/page.tsx
+++ app/(main)/history/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState } from 'react';
-import { FilterMatchMode } from 'primereact/api';
-import { DataTable } from 'primereact/datatable';
+import { FilterMatchMode, FilterOperator  } from 'primereact/api';
+import { DataTable, DataTableFilterMeta } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
@@ -14,27 +14,64 @@ export default function BasicFilterDemo() {
         { id: 1002, name: 'Process C', startTime: '2024-10-22T17:15:00', duration: 11700 },
     ];
 
-    const [globalFilter, setGlobalFilter] = useState<string | null>(null);
-    const [filters, setFilters] = useState({
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-    });
+    const [globalFilterValue1, setGlobalFilterValue1] = useState('');
     const [loading, setLoading] = useState(false);
     const [selectedProcesses, setSelectedProcesses] = useState<any[]>([]);
+    const [filters1, setFilters1] = useState<DataTableFilterMeta>({});
+    
+    const clearFilter1 = () => {
+        initFilters1();
+    };
 
-    const graphBodyTemplate = () => {
+    const onGlobalFilterChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        let _filters1 = { ...filters1 };
+        (_filters1['global'] as any).value = value;
+
+        setFilters1(_filters1);
+        setGlobalFilterValue1(value);
     };
     
+    const initFilters1 = () => {
+        setFilters1({
+            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            name: {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
+            },
+            'country.name': {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
+            },
+            representative: { value: null, matchMode: FilterMatchMode.IN },
+            date: {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }]
+            },
+            balance: {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }]
+            },
+            status: {
+                operator: FilterOperator.OR,
+                constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }]
+            },
+            activity: { value: null, matchMode: FilterMatchMode.BETWEEN },
+            verified: { value: null, matchMode: FilterMatchMode.EQUALS }
+        });
+        setGlobalFilterValue1('');
+    };
+
     const renderHeader1 = () => {
         return (
             <div className="flex justify-content-between">
-                <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined />
+                <Button type="button" icon="pi pi-filter-slash" label="Obriši filter" outlined onClick={clearFilter1}/>
                 <div className='flex justify-content-between gap-3'>
                 <Button icon="pi pi-print" className="p-button-text p-button-plain" size='large'/>
                 <Button icon="pi pi-chart-line" className="p-button-text p-button-plain" size='large'/>
                 <span className="p-input-icon-left">
                     <i className="pi pi-search" />
-                    <InputText placeholder="Filtriraj procese" />
+                    <InputText value={globalFilterValue1} onChange={onGlobalFilterChange1} placeholder="Filtriraj procese" />
                 </span>
                 </div>
             </div>
