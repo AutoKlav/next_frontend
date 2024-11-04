04074198daf9e707f5723339e1f53dc63332209d
Spike433 <juhacar116@gmail.com>
2024-11-04 14:17:21 +0100

    Remove unused code and console.log statement

--- app/(main)/chart/page.tsx
+++ app/(main)/chart/page.tsx
@@ -1,17 +1,6 @@
 import LineChart from '@/demo/components/Charts/LineChart';
 import React from 'react';
 
-export async function getServerSideProps() {
-    const res = await fetch('http://localhost:3000/api/getStatus');
-    const status = await res.json();
-    console.log("Hey", status);
-    return {
-        props: {
-            status,
-        },
-    };
-}
-
 const ChartPage = () => {
 
     return (
