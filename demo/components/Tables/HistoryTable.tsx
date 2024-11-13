ca24cd69dada308aa77defd8beff24ac2ab10f59
Spike433 <juhacar116@gmail.com>
2024-11-13 10:59:50 +0100

    Remove unnecessary API call and add data fetching with react-query

--- demo/components/Tables/HistoryTable.tsx
+++ demo/components/Tables/HistoryTable.tsx
@@ -7,13 +7,23 @@ import { Column, ColumnFilterElementTemplateOptions } from 'primereact/column';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { Calendar } from 'primereact/calendar';
+import { useQuery } from '@tanstack/react-query';
+import { getProcessesAction } from '@/app/(main)/api/actions';
 
-const HistoryTable = () => {
+const HistoryTable = () => {    
     const processes = [
         { id: 1000, name: 'Process A', startTime: '2024-10-22T15:05:20', duration: 9000 },
         { id: 1001, name: 'Process B', startTime: '2024-10-22T16:00:00', duration: 6300 },
         { id: 1002, name: 'Process C', startTime: '2024-10-22T17:15:00', duration: 11700 },
     ];
+
+    const { data: processesData } = useQuery(
+        { 
+            queryKey: ['processesData'],
+            queryFn: () => getProcessesAction(),            
+            refetchInterval: 1000,                        
+        },        
+    );    
 
     const [globalFilterValue1, setGlobalFilterValue1] = useState('');
     const [loading, setLoading] = useState(false);
