02215a8c686f80b64a6c874ef75c2a5f44abed5c
Spike433 <juhacar116@gmail.com>
2024-11-19 21:53:46 +0100

    Add useMutation and useQuery hooks to HistoryTable

--- demo/components/Tables/HistoryTable.tsx
+++ demo/components/Tables/HistoryTable.tsx
@@ -7,8 +7,12 @@ import { Column } from "primereact/column";
 import { InputText } from "primereact/inputtext";
 import { Calendar } from "primereact/calendar";
 import { Button } from "primereact/button";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getProcessLogsAction, getProcessesAction } from "@/app/(main)/api/actions";
 
 const HistoryTable = () => {
+    const [loading, setLoading] = useState(true);
+
     const processesDataQuery = [
         {
             id: 1,
@@ -36,8 +40,22 @@ const HistoryTable = () => {
         },        
     ];
 
+    const { data: processesDataQuery1 } = useQuery({
+        queryKey: ['processesDataQuery'],
+        queryFn: () => getProcessesAction(),
+        onSuccess: () => setLoading(false),
+        onError: () => setLoading(false)
+    });
+
+    const {isLoading, mutate: getProcessLogMutation} = useMutation(getProcessLogsAction, {
+        onSuccess: (response) => {           
+            //console.log('Process logs:', response.processlogsList);
+        }
+    });
+
     const [globalFilterValue, setGlobalFilterValue] = useState("");
     const [filters, setFilters] = useState<DataTableFilterMeta | undefined> (undefined);
+    const [selectedProcesses, setSelectedProcesses] = useState<any[]>([]);
 
     useEffect(() => {
         initFilters();
@@ -45,18 +63,10 @@ const HistoryTable = () => {
 
     const initFilters = () => {
         setFilters({
-            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-            productname: {
-                operator: FilterOperator.AND,
-                constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-            },
+            global: { value: null, matchMode: FilterMatchMode.CONTAINS },          
             processstart: {
                 operator: FilterOperator.AND,
                 constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }],
-            },
-            processlength: {
-                operator: FilterOperator.AND,
-                constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
             },
         });
     };
@@ -80,22 +90,23 @@ const HistoryTable = () => {
         setGlobalFilterValue(value);
     };
 
+    const clearFilter = () => {
+        initFilters();
+    };
+
     const renderHeader = () => {
         return (
             <div className="flex justify-content-between">
-                <Button
-                    type="button"
-                    icon="pi pi-filter-slash"
-                    label="Clear"
-                    outlined
-                    onClick={initFilters}
-                />
-                <InputText
-                    value={globalFilterValue}
-                    onChange={onGlobalFilterChange}
-                    placeholder="Filter processes"
-                    style={{ borderRadius: "11px" }}
-                />
+                <Button type="button" icon="pi pi-filter-slash" label="Obriši filter" outlined onClick={clearFilter} />
+                <div className='flex justify-content-between gap-3'>
+                    {selectedProcesses.length > 0 && (
+                        <>
+                            <Button icon="pi pi-print" className="p-button-text p-button-plain" size='large' onClick={handlePrint} />
+                            <Button icon="pi pi-chart-line" className="p-button-text p-button-plain" size='large' onClick={handleGraph} />
+                        </>
+                    )}
+                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Filtriraj procese" style={{ borderRadius: '11px' }} />
+                </div>
             </div>
         );
     };
@@ -117,7 +128,19 @@ const HistoryTable = () => {
 
     const dateBodyTemplate = (rowData: any) => {
     return formatDate(rowData.processstart); // Safe formatting
-};
+    };
+
+    const handlePrint = () => {
+        console.log('Print processes:', selectedProcesses);
+        // Add your print logic here
+    };
+
+    const handleGraph = () => {
+        console.log('Graph processes:', selectedProcesses);
+        const identifiers = selectedProcesses.map((process) => process.id);        
+        getProcessLogMutation(identifiers);        
+    };
+
 
     const header = renderHeader();
 
@@ -125,16 +148,22 @@ const HistoryTable = () => {
         <div className="card">
             <h2>Process History</h2>
             <DataTable
+                className="p-datatable-gridlines"
+                showGridlines
                 value={processesDataQuery}
                 paginator
                 rows={5}
                 dataKey="id"
                 filters={filters}
+                selection={selectedProcesses}
+                onSelectionChange={(e) => setSelectedProcesses(e.value)}
                 filterDisplay="menu"
-                globalFilterFields={["productname", "processstart"]}
+                globalFilterFields={["productname","processstart"]}
                 header={header}
                 emptyMessage="No processes found."
             >
+                <Column selectionMode="multiple" headerStyle={{ width: '3em' }} />
+                
                 <Column
                     field="productname"
                     header="Process Name"                    
@@ -147,6 +176,7 @@ const HistoryTable = () => {
                     filter
                     filterElement={dateFilterTemplate}
                     showFilterMatchModes
+                    style={{ maxWidth: '200px' }} 
                 />
                 <Column field="processlength" header="Process Length (s)" />
             </DataTable>
