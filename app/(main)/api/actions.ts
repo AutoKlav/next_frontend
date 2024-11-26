f7d97c6d9a8820c20c09156efda6f0d1fd6e34cf
Spike433 <juhacar116@gmail.com>
2024-11-26 23:37:40 +0100

    Add SensorRelayValues interface and API endpoint

--- app/(main)/api/actions.ts
+++ app/(main)/api/actions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { getAllProcessLogs, getAllProcesses, getSensorValues, getStateMachineValues, getStatus, getVariables, setVariable, startProcess, stopProcess, updateSensor } from "@/services/grpc";
+import { getAllProcessLogs, getAllProcesses, getSensorRelayValues, getSensorValues, getStateMachineValues, getStatus, getVariables, setVariable, startProcess, stopProcess, updateSensor } from "@/services/grpc";
 import { ProcessConfigMode, ProcessConfigType, StartProcessRequest } from "@/types/grpc";
 
 //#region GET Actions
@@ -17,6 +17,11 @@ export const getStateMachineValuesAction = async () => {
 
 export const getSensorValuesAction = async () => {
     const sensors = await getSensorValues();
+    return sensors;
+}
+
+export const getSensorRelayValuesAction = async () => {
+    const sensors = await getSensorRelayValues();
     return sensors;
 }
 
