225896f2fa5e360c1a293509c476e2d4c6faaa50
Spike433 <juhacar116@gmail.com>
2024-11-28 20:24:40 +0100

    Add useMutation hook and fetch process logs

--- demo/components/Charts/MultiYAxisChart.tsx
+++ demo/components/Charts/MultiYAxisChart.tsx
@@ -5,12 +5,21 @@ import { Chart } from "primereact/chart";
 import { Button } from "primereact/button";
 import { ChartOptions } from "chart.js";
 import { handleExportToPDF } from "@/utils/exportUtil";
+import { useMutation } from "@tanstack/react-query";
+import { getProcessLogsAction } from "@/app/(main)/api/actions";
 
 const MultiYAxisChart = () => {
     const [chartData, setChartData] = useState({});
     const [chartOptions, setChartOptions] = useState<ChartOptions<"line">>({});
     const chartRef = useRef<any>(null);
 
+    const { isLoading: isLogLoading, mutate: getProcessLogMutation } = useMutation(getProcessLogsAction, {
+        onSuccess: ({data, source}) => {
+            
+            console.log("Process logs:", data.processlogsList);            
+        },
+    });
+    
     useEffect(() => {
         const updateChartOptions = (textColor: string, gridColor: string): ChartOptions<"line"> => ({
             maintainAspectRatio: true,
@@ -35,45 +44,52 @@ const MultiYAxisChart = () => {
                 y: {
                     type: "linear",
                     position: "left",                    
-                    ticks: { color: textColor },                    
+                    ticks: { color: textColor, stepSize: 11 },                    
                     grid: {  color: gridColor, drawOnChartArea: true },
                 },
                 y1: {
                     type: "linear",
                     position: "left",
                     offset: true,
-                    ticks: { color: textColor },
+                    ticks: { color: textColor, stepSize: 14 },
                     grid: {  color: gridColor, drawOnChartArea: true },
                 },
                 y2: {
                     type: "linear",
                     position: "right",
-                    ticks: { color: textColor },
+                    ticks: { color: textColor, stepSize: 16 },
                     grid: { color: gridColor, drawOnChartArea: true },
                 },
                 y3: {
                     type: "linear",
                     position: "right",
                     offset: true,
-                    ticks: { color: textColor },
+                    ticks: { color: textColor, stepSize: 9 },
                     grid: { color: gridColor, drawOnChartArea: true },
                 },
                 y4: {
                     type: "linear",
                     position: "right",
                     offset: true,
-                    ticks: { color: textColor },
+                    ticks: { color: textColor, stepSize:12 },
                     grid: { color: gridColor, drawOnChartArea: true },
                 },
             },
         });
 
         const data = {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+            labels: [
+                "11:52:53", 
+                "11:53:49", 
+                "12:29:49", 
+                "14:09:46", 
+                "13:47:46", 
+                "13:49:46"
+            ],
             datasets: [
                 {
-                    label: "Sales (2023)",
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    label: "Temperatura vode",
+                    data: [11.7276, 11.7276, 60.9079, 60.4035, 116.898, 116.898],                 
                     fill: false,
                     borderColor: "rgba(75, 192, 192, 1)", // Teal
                     backgroundColor: "rgba(75, 192, 192, 0.3)", // Soft fill
@@ -87,8 +103,8 @@ const MultiYAxisChart = () => {
                     yAxisID: "y1",
                 },
                 {
-                    label: "Revenue (2023)",
-                    data: [90, 85, 75, 95, 105, 110, 115],
+                    label: "Temperatura konzerve",
+                    data: [12.7364, 12.7364, 45.0189, 105.044, 113.871, 114.124],
                     fill: false,
                     borderColor: "rgba(255, 99, 132, 1)", // Red
                     backgroundColor: "rgba(255, 99, 132, 0.3)", // Soft fill
@@ -102,8 +118,23 @@ const MultiYAxisChart = () => {
                     yAxisID: "y",
                 },
                 {
-                    label: "Expenses",
-                    data: [45, 50, 55, 50, 60, 65, 70],
+                    label: "Tlak",
+                    data: [108.364, 108.364, 76.0811, 16.0559, 7.2287, 6.97649],
+                    fill: false,
+                    borderColor: "rgba(153, 102, 255, 1)", // Purple
+                    backgroundColor: "rgba(153, 102, 255, 0.3)", // Soft fill
+                    borderDash: [5, 10, 2, 10], // Dash-dot pattern
+                    borderWidth: 4,
+                    pointStyle: "crossRot",
+                    pointBorderColor: "rgba(153, 102, 255, 1)",
+                    pointBackgroundColor: "rgba(255, 255, 255, 1)", // White fill
+                    pointRadius: 6,
+                    tension: 0.1,
+                    yAxisID: "y2",
+                },
+                {
+                    label: "Fr",
+                    data: [-0.0341854, -0.00335308, 1.74381, 1.8089, 2.04871, 2.05214],
                     fill: false,
                     borderColor: "rgba(54, 162, 235, 1)", // Blue
                     backgroundColor: "rgba(54, 162, 235, 0.3)", // Soft fill
@@ -117,8 +148,8 @@ const MultiYAxisChart = () => {
                     yAxisID: "y2",
                 },
                 {
-                    label: "Profit",
-                    data: [10, 15, 20, 25, 30, 35, 40],
+                    label: "sumFr",
+                    data: [0.0000000000728809, 0.000000000145762, 0.00000012327, 0.123987, 0.946455, 1.00305],                 
                     fill: false,
                     borderColor: "rgba(255, 205, 86, 1)", // Yellow
                     backgroundColor: "rgba(255, 205, 86, 0.4)", // Transparent fill
@@ -130,22 +161,7 @@ const MultiYAxisChart = () => {
                     pointRadius: 5,
                     tension: 0.5,
                     yAxisID: "y3",
-                },
-                {
-                    label: "Growth Rate",
-                    data: [5,5, 25, 20, 65, 30, 35],
-                    fill: false,
-                    borderColor: "rgba(153, 102, 255, 1)", // Purple
-                    backgroundColor: "rgba(153, 102, 255, 0.3)", // Soft fill
-                    borderDash: [5, 10, 2, 10], // Dash-dot pattern
-                    borderWidth: 4,
-                    pointStyle: "crossRot",
-                    pointBorderColor: "rgba(153, 102, 255, 1)",
-                    pointBackgroundColor: "rgba(255, 255, 255, 1)", // White fill
-                    pointRadius: 6,
-                    tension: 0.1,
-                    yAxisID: "y4",
-                },
+                },              
             ],
         };        
 
@@ -157,6 +173,7 @@ const MultiYAxisChart = () => {
 
     return (
         <div className="card">
+            <Button label="Fetch" onClick={() => getProcessLogMutation({ ids:[55], source: "graph" })} className="p-button-success" />
             <Chart ref={chartRef} type="line" data={chartData} options={chartOptions} />
             <Button label="Export to PDF" onClick={() => handleExportToPDF(chartRef, chartOptions)} className="p-button-info mt-5" />
         </div>
