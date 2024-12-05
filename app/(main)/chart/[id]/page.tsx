59e67d947a244e7422a671202a9e1e081e5f0dde
Spike433 <juhacar116@gmail.com>
2024-12-05 07:03:16 +0100

    Fix API actions and add data query in chart page

--- app/(main)/chart/[id]/page.tsx
+++ app/(main)/chart/[id]/page.tsx
@@ -1,13 +1,35 @@
 "use client";
 
 import { MultiYAxisChart } from '@/demo/components/Charts/MultiYAxisChart';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import React from 'react';
+import { getProcessesAction } from '../../api/actions';
+import { useToast } from '@/layout/context/toastcontext';
 
 const ChartPage = () => {
+    const { showError } = useToast();
     const { id } = useParams();
-    
-    console.log("ID:", id);
+
+    const { data: filteredProcessQuery, isLoading: loading } = useQuery({
+        queryKey: ["processesDataQuery"],
+        queryFn: async () => {
+            const response =  await getProcessesAction();
+            
+            console.log("Processes:", response?.processesList);
+            
+            return response?.processesList?.filter((process) => process.id.toString() === id);
+        },
+        onError(err) {
+            showError(
+                "Greška",
+                "Nije moguće dohvatiti podatke sa senzora. Provjerite konekciju i pokušajte ponovno."
+            );
+            console.log(err);
+        },
+    });
+
+    console.log("ProcessesDataQuery:", filteredProcessQuery);
 
     const chartInfo = {
         id: 55,
@@ -17,7 +39,7 @@ const ChartPage = () => {
     
     return (
         <div className='grid'>            
-            <div className='col-12'>
+            <div className='col-12'>            
                 <MultiYAxisChart id={chartInfo.id} title={chartInfo.title} subtitle={chartInfo.subtitle} />
             </div>
         </div>
