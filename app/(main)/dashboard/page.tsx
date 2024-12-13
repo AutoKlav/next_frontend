6de3bef48f14e2fa9272bc1d297da8524fcaaa02
Spike433 <juhacar116@gmail.com>
2024-12-13 22:22:46 +0100

    Update typeDropdown initialization

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -91,7 +91,7 @@ const DashboardPage = () => {
         setFinishTemp(0);
         setMaintainPressure(0);        
         setMaintainTemp(0);
-        setTypeDropdown(modeDropdownValues[0]);
+        setTypeDropdown(fetchedTypes.current?.[0]);
         //#endregion
         
         //#region typeDropdown
