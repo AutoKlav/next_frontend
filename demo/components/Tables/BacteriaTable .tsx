c332508563719b9087bb646e489cb1d6d65a5b42
Spike433 <juhacar116@gmail.com>
2025-03-17 23:07:55 +0100

    Refactor BacteriaTable component to include delete functionality

--- demo/components/Tables/BacteriaTable .tsx
+++ demo/components/Tables/BacteriaTable .tsx
@@ -2,26 +2,30 @@
 import React, { useRef, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { InputNumber } from 'primereact/inputnumber';
+import { Button } from 'primereact/button';
 import { CSSProperties } from 'react';
-import { setVariableAction } from '@/app/(main)/api/actions';
-import { BacteriaList, SetVariable, } from '@/types/grpc';
-import { useMutation } from '@tanstack/react-query';
+import { BacteriaList } from '@/types/grpc';
 import { Toast } from 'primereact/toast';
 
-const BacteriaTable  = (bacteria: BacteriaList) => {
-    const [config, setConfig] = useState([
-        ...bacteria.bacteriaList
-    ]);
-
+const BacteriaTable = (bacteria: BacteriaList) => {
+    const [config, setConfig] = useState([...bacteria.bacteriaList]);
     const toast = useRef<Toast>(null);
-    console.log(bacteria);
+
+    const deleteRow = (id: string) => {
+        setConfig(prevConfig => prevConfig.filter(item => item.id.toString() !== id));
+        toast.current?.show({ severity: 'warn', summary: 'Deleted', detail: 'Row has been deleted', life: 3000 });
+    };
+
     const columns = [
-        { field: 'name', header: 'Ime', editable: false },
-        { field: 'description', header: 'Opis', editable: false },
-        { field: 'd0', header: 'd0', editable: false },
-        { field: 'z', header: 'z', editable: false }
+        { field: 'name', header: 'Ime' },
+        { field: 'description', header: 'Opis'},
+        { field: 'd0', header: 'd0' },
+        { field: 'z', header: 'z' }
     ];   
+
+    const deleteButton = (rowData: any) => {
+        return <Button icon="pi pi-trash" className="p-button-rounded" onClick={() => deleteRow(rowData.id)} />;
+    };
 
     return (
         <div className="card p-fluid">
@@ -29,20 +33,18 @@ const BacteriaTable  = (bacteria: BacteriaList) => {
             <h2 style={{ textAlign: 'left' }}>Globalne varijable</h2>
             <DataTable 
                 dataKey="id" 
-                editMode="row" 
                 value={config}                 
                 loading={false}
                 tableStyle={{ minWidth: '50rem' }}>
-                {columns.map(({ field, header }) => {
-                    const columnProps = {
-                        field: field,
-                        header: header,
-                        style: { width: '50%', textAlign: 'left' as CSSProperties['textAlign'] },
-                    };
-
-                    return <Column key={field} {...columnProps} />;
-                })}
-                <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
+                {columns.map(({ field, header }) => (
+                    <Column 
+                        key={field} 
+                        field={field} 
+                        header={header} 
+                        style={{ width: '50%', textAlign: 'left' as CSSProperties['textAlign'] }} 
+                    />
+                ))}
+                <Column body={deleteButton} headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }} />
             </DataTable>
         </div>
     );
