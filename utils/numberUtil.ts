c46d653d0610d013cc468c00379caba96d8a33b9
Spike433 <juhacar116@gmail.com>
2025-08-12 22:42:18 +0200

    fix: update exponential notation to display 1 decimal place for values above 10,000

--- utils/numberUtil.ts
+++ utils/numberUtil.ts
@@ -1,14 +1,14 @@
 /**
  * Parses a string value into a number and formats it for display.
  * If the parsed value is between 0 and 10,000 (inclusive), it is displayed in normal notation with 2 decimal places.
- * Otherwise, it is displayed in exponential notation with 2 decimal places.
+ * Otherwise, it is displayed in exponential notation with 1 decimal place.
  * If the value is NaN, it displays 'Nedostupno'.
  * If the value is Infinity, it displays 'Beskonačno'.
  * 
  * @param value - The string value to parse and format.
  * @returns The formatted display value as a string.
  */
-export const parseAndFormatValue = (value: string, decimal: number = 2): string => {
+export const parseAndFormatValue = (value: string, decimal: number = 1): string => {
     const parsedValue = parseFloat(value);
     let displayValue;
 
